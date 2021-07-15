d5f86c3c3bc868de98dbacd83031d9b89e783548
Jean-Claude Parent <noursicain@gmail.com>
2021-07-14 20:48:03 -0400

    Création d'un producteur simple sur windows

---
+++ app/producteursimple/producteursimple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace producteursimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A faire produire avec amqp net lite");
+        }
+    }
+}
